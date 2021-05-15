9a690f558492dd70f69f49ea0fd71b7d65b4f864
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2021-05-15 18:03:53 +0530

    backend setup

---
+++ e-commerce-server/src/main/java/com/codelabs/ecommerce/ECommerceServerApplication.java
@@ -0,0 +1,13 @@
+package com.codelabs.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceServerApplication.class, args);
+	}
+
+}
